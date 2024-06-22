a3c94abe9564e843a94b511a09fcbda660d3349e
Juan Avero <averojuanm@gmail.com>
2024-06-22 13:08:42 -0300

    Create .sql example files to populate database with data

---
+++ sql/insert_initial_movies.sql
@@ -0,0 +1,10 @@
+/*
+Run to insert initial movies data.
+It is not necessary to run it mandatorily.
+*/
+INSERT INTO movie (id, created_at, title, director, opening_crawl, release_date) VALUES
+(1, now(), 'Star Wars Ep. I: The Phantom Menace', 'George Lucas', 'A long time ago in a galaxy far, far away....', '1999-05-19'),
+(2, now(), 'Star Wars Ep II: Attack of the Clones', 'George Lucas', 'A long time ago in a galaxy far, far away....', '2002-05-16'),
+(3, now(), 'Star Wars Ep III: Revenge of the Sith', 'George Lucas', 'A long time ago in a galaxy far, far away....', '2005-05-19'),
+(4, now(), 'Star Wars Ep IV: A New Hope', 'George Lucas', 'A long time ago in a galaxy far, far away....', '1977-05-25'),
+(5, now(), 'Star Wars Ep V: The Empire Strikes Back', 'Irvin Kershner', 'A long time ago in a galaxy far, far away....', '1980-05-21');
