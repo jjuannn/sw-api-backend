e2924ff396205808826583b8065d74fba1b1872f
Juan Avero <64806950+jjuannn@users.noreply.github.com>
2025-08-29 12:33:41 -0300

    Move Encrypt as submodule of Common module

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from './controller/jwt/jwt.strategy';
+import { EncryptService } from './service/encrypt.service';
 
 @Module({
-  providers: [JwtStrategy],
+  providers: [JwtStrategy, EncryptService],
+  exports: [EncryptService],
 })
 export class CommonModule {}
