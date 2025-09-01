7226536b19dfaceba7ec19019030fbed74ff0e0a
Juan Avero <64806950+jjuannn@users.noreply.github.com>
2025-09-01 00:01:17 -0300

    Update .env.dist and user admin script with default local value for JWT_SECRET

--- sql/insert_admin_user.sql
+++ sql/insert_admin_user.sql
@@ -3,5 +3,6 @@ Run to insert an ADMIN user with the credentials
  - email 'admin@example.com'
  - password 'password'
 Required to perform ADMIN operations
+NOTE: Works only using the JWT_SECRET specified in the .env.dist file 
 */
 INSERT INTO "user" (email, role, password, created_at) VALUES ('admin@example.com', 'ADMIN', '$2b$10$pDUcAEeRnYsBoLDxeimZhu6dvPVxWd9RLol2wRJI5nDKcmVAyrMkm', now())
